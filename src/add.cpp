9f0fe55226860130adbb1e91e7ff833d43a6f4e3
StoneRobot <1107981147@qq.com>
2020-05-26 08:40:43 +0800

    为障碍物添加增加publisher

--- src/add.cpp
+++ src/add.cpp
@@ -8,7 +8,8 @@ int main(int argc, char *argv[])
     ros::init(argc, argv, "add");
     ros::NodeHandle nh;
     moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-    addCollisionObjects(planning_scene_interface, 
+    ros::Publisher planning_scene_diff_publisher = nh.advertise<moveit_msgs::PlanningScene>("planning_scene", 1);
+    addCollisionObjects(planning_scene_interface, planning_scene_diff_publisher,
     atof(argv[1]), atof(argv[2]), atof(argv[3]), 
     atof(argv[4]), atof(argv[5]), atof(argv[6]), 
     atof(argv[7]), atof(argv[8]), atof(argv[9]), 
